fdd61fda5efbac59af45f872236e09efb7e8c695
Oreginal1 <eagle_lens@yahoo.com>
2023-02-17 13:20:21 -0800

    4-put.c

--- 0x00-hello_world/4-put.c
+++ 0x00-hello_world/4-put.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'It print a string without using printf'
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main()
+int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
